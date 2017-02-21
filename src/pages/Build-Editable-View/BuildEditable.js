039b578ddc52bd205671909cb58875c44a9d0c52
Corey Burkhart <hello@coreyburkhart.com>
2017-02-20 16:30:59 -0800

    add builders structure to store

--- src/pages/Build-Editable-View/BuildEditable.js
+++ src/pages/Build-Editable-View/BuildEditable.js
@@ -8,14 +8,15 @@ import FloatButton from '../../Components/FloatButton.js';
 class BuildEditable extends React.Component {
 
   render() {
-    const { title, gallery } = this.props.route.store;
+    const { title, builders, gallery } = this.props.route.store;
     return (
       <div id='build-editable-wrapper'>
         <header id='build-header'>
           <h2  className='build-name'>title of build</h2>
           <div className="builders-div">
-            <img className='builder' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Reproduction-of-the-1805-Rembrandt-Peale-painting-of-Thomas-Jefferson-New-York-Historical-Society_1.jpg/800px-Reproduction-of-the-1805-Rembrandt-Peale-painting-of-Thomas-Jefferson-New-York-Historical-Society_1.jpg' alt='builder name'/>
-            <img className='builder' src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Portrait_of_George_Washington-transparent.png/800px-Portrait_of_George_Washington-transparent.png' alt='builder name'/>
+            {builders.map((builder) => {
+              return <img className='builder' key={builder.name} src={builder.thumb} alt={builder.name}/>
+            })}
             <FloatButton id='add-builder' className='builder'/>
           </div>
           <i className="material-icons build-menu">more_vert</i>
@@ -25,7 +26,11 @@ class BuildEditable extends React.Component {
             {gallery.map((href) => {
               return (<figure key={href} className='gallery-fig' ><img src={href} alt='something'/><i className='material-icons close'>close</i></figure>)
             })}
-            <FloatButton id='add-image' icon='edit' onClick={()=>{console.log('add img')} }/>
+            <div className='panel-options-div'>
+              <FloatButton id='add-image' icon='add' onClick={()=>{console.log('add img')} }/>
+              <FloatButton id='add-image' icon='close' onClick={()=>{console.log('add img')} }/>
+              <FloatButton id='add-image' icon='delete' onClick={()=>{console.log('add img')} }/>
+            </div>
           </div>
         </section>
       </div>
--- src/pages/Build-Editable-View/BuildEditableStore.js
+++ src/pages/Build-Editable-View/BuildEditableStore.js
@@ -1,7 +1,34 @@
 import { observable } from 'mobx';
 
+/*
+Model:
+{
+  title: String,
+  builders: [...{thumb: String, name: String}]
+  gallery_thumbs: [...String],
+  gallery_src: [...String]
+}
+
+*/
+
+
+
+
+
+
+
+
 class BuildEditableStore {
   @observable title = 'My Tiny House Build';
+  @observable builders = [{
+      name: 'George',
+      thumb:      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Reproduction-of-the-1805-Rembrandt-Peale-painting-of-Thomas-Jefferson-New-York-Historical-Society_1.jpg/800px-Reproduction-of-the-1805-Rembrandt-Peale-painting-of-Thomas-Jefferson-New-York-Historical-Society_1.jpg'
+    },
+    {
+      name: 'Bob',
+      thumb: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Portrait_of_George_Washington-transparent.png/800px-Portrait_of_George_Washington-transparent.png'
+    }
+]
   @observable gallery = ['http://www.wakingtimes.com/wp-content/uploads/2013/02/Tiny-House-at-Night.jpg', 'http://www.wakingtimes.com/wp-content/uploads/2015/04/glamper-tiny-house-camper-4.jpg', 'https://www.tumbleweedhouses.com/wp-content/uploads/tumbleweed-tiny-house-elm-0007.jpg', 'https://www.tumbleweedhouses.com/wp-content/uploads/tumbleweed-tiny-house-elm-0010.jpg', 'https://www.tumbleweedhouses.com/wp-content/uploads/tumbleweed-tiny-house-elm-0014.jpg']
 }
 
