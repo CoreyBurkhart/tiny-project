d84fff067ec3f32d48f43418197d37cfbbf9583e
Corey Burkhart <hello@coreyburkhart.com>
2017-02-21 14:48:24 -0800

    added info modal to summary

--- src/pages/Build-Editable-View/Components/Summary.js
+++ src/pages/Build-Editable-View/Components/Summary.js
@@ -8,6 +8,12 @@ class Summary extends React.Component {
 
     return (
       <section id='summary' className='panel'>
+        <p className='summary-description'>
+          <i className='material-icons info'>info</i> This is the part where you talk about you and you build. If you are making your build purchasable, here would be a great place to sell features and content!
+
+          This summary utilizes markdown, allowing for detailed description and style. Learn more about it <a href='#' >here</a>.
+        </p>
+        <div className='divider'></div>
         <div className='summary-content'>
           <textarea id='summary-input' placeholder='Tell everybody about your awesome build!' defaultValue={summary}/>
           {/* //saved indicator */}
